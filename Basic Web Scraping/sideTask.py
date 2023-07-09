ac3f450cca4c79d51fcc10565ba48232850d9114
Hamid Muzaffar <hamidmuzaffar49@gmail.com>
2023-07-09 12:50:06 +0500

    Adding Dimension

--- Basic Web Scraping/sideTask.py
+++ Basic Web Scraping/sideTask.py
@@ -4,46 +4,48 @@ import re
 from linksList import links
 import pandas as pd
 
+# Function to send asynchronous HTTP requests to multiple URLs
 def get_data(urls):
     reqs = [grequests.get(link) for link in urls]
     resp = grequests.map(reqs)
     return resp
 
+# Function to parse the HTML responses and extract data
 def parseData(responses):
     data = []
     for response in responses:
-        sp = bs(response.text, 'lxml')
-        product_url = response.url
-        category = sp.find('div', {'class': 'breadcrumb-v2'})
-        category_1 = re.sub(r'^\s+|\s+$', '', category.find_all('li')[2].text)
-        category_2 = re.sub(r'^\s+|\s+$', '', category.find_all('li')[3].text)
-        series = sp.find('h2', {'class': 'coleccion-name'}).text
-        name = sp.find('h1', {'id': 'prod-name'}).text
-        article = sp.find('p', {'id': 'prod-ref'}).text
-        articleNum = re.sub(' ', '', article.split(':')[1])
-        dim = sp.find('span', {'id': 'dim-txt'}).text
-        matches = re.findall(r'\d+', dim)
+        sp = bs(response.text, 'lxml')  # Create BeautifulSoup object to parse HTML
+        product_url = response.url  # Get the URL of the current product
+        category = sp.find('div', {'class': 'breadcrumb-v2'})  # Find the category information in the HTML
+        category_1 = re.sub(r'^\s+|\s+$', '', category.find_all('li')[2].text)  # Extract the first category
+        category_2 = re.sub(r'^\s+|\s+$', '', category.find_all('li')[3].text)  # Extract the second category
+        series = sp.find('h2', {'class': 'coleccion-name'}).text  # Extract the series name
+        name = sp.find('h1', {'id': 'prod-name'}).text  # Extract the product name
+        article = sp.find('p', {'id': 'prod-ref'}).text  # Extract the article number
+        articleNum = re.sub(' ', '', article.split(':')[1])  # Clean the article number
+        dim = sp.find('span', {'id': 'dim-txt'}).text  # Extract the dimensions
+        matches = re.findall(r'\d+', dim)  # Extract numerical values from the dimensions
         if len(matches) >= 3:
             length = matches[0]
             width = matches[1]
             height = matches[2]
-        color = re.sub(r'\s', '', sp.find('span', {'id': f'ncollapse{articleNum}'}).text)
-        type = sp.find(lambda tag: tag.name == "option" and tag.has_attr("selected"))
-        type = '' if type is None else re.sub(r'\s', '', type.text)
-        main_img = sp.find('img', {'data-finish': f'n-finished-{articleNum}'})
-        main_img = sp.find(lambda tag: tag.name == "img" and tag.has_attr("data-finish"))['src'] if main_img is None else main_img['src']
-        gallery = sp.find('div', {'id': 'productDetailThumbnailSlider'})
-        gallery_images = list(map(lambda tag: tag['src'], gallery.find_all(lambda tag: tag.name == 'img' and f'n-finished-{articleNum}' not in tag.get('data-finish'))))
-        short_description_section = sp.find('section', {'id': 'anclaProductFeat'})
-        material = short_description_section.find('p', {'data-code': 'Material'}) 
-        material = '' if material is None else re.sub('Material: ', '', material.text)
-        short_description = list(map(lambda element: element.text, short_description_section.find_all('p')))
-        data_sheet = list(map(lambda element: element['href'], sp.find_all('a', {'class': 'descarga'})))
-        data_file_3d = sp.find('a', {'class': 'icon-stp'})
-        data_file_3d = '' if data_file_3d is None else data_file_3d['href']
-        long_description = sp.find('p', {'class': 'clamp'})
+        color = re.sub(r'\s', '', sp.find('span', {'id': f'ncollapse{articleNum}'}).text)  # Extract the color
+        type = sp.find(lambda tag: tag.name == "option" and tag.has_attr("selected"))  # Find the selected option (type)
+        type = '' if type is None else re.sub(r'\s', '', type.text)  # Extract the type
+        main_img = sp.find('img', {'data-finish': f'n-finished-{articleNum}'})  # Find the main image
+        main_img = sp.find(lambda tag: tag.name == "img" and tag.has_attr("data-finish"))['src'] if main_img is None else main_img['src']  # Extract the main image URL
+        gallery = sp.find('div', {'id': 'productDetailThumbnailSlider'})  # Find the image gallery
+        gallery_images = list(map(lambda tag: tag['src'], gallery.find_all(lambda tag: tag.name == 'img' and f'n-finished-{articleNum}' not in tag.get('data-finish'))))  # Extract gallery image URLs
+        short_description_section = sp.find('section', {'id': 'anclaProductFeat'})  # Find the short description section
+        material = short_description_section.find('p', {'data-code': 'Material'})  # Find the material information
+        material = '' if material is None else re.sub('Material: ', '', material.text)  # Extract the material
+        short_description = list(map(lambda element: element.text, short_description_section.find_all('p')))  # Extract short description paragraphs
+        data_sheet = list(map(lambda element: element['href'], sp.find_all('a', {'class': 'descarga'})))  # Extract data sheet URLs
+        data_file_3d = sp.find('a', {'class': 'icon-stp'})  # Find the 3D data file
+        data_file_3d = '' if data_file_3d is None else data_file_3d['href']  # Extract the 3D data file URL
+        long_description = sp.find('p', {'class': 'clamp'})  # Find the long description
         if long_description:
-            lD = long_description.text
+            lD = long_description.text  # Extract the long description
 
         # Store the data in a dictionary
         data.append({
@@ -53,6 +55,9 @@ def parseData(responses):
             'Name': name,
             'Color': color,
             'Type': type,
+            'lenght': length,
+            'width': width,
+            'height': height,
             'Main Image': main_img,
             'Gallery Images': gallery_images,
             'Material': material,
@@ -68,6 +73,8 @@ def parseData(responses):
     # Save the DataFrame to an Excel file
     df.to_excel('output.xlsx', index=False)
 
-
+# Get the data from the provided links
 resp = get_data(links)
+
+# Parse the data and save it to an Excel file
 parseData(resp)
